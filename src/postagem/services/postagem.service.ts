1ddf0dc5c6c2575048f28840bbeea08f29430062
ravila joana <ravilajoanasousapereira@gmail.com>
2025-09-25 12:32:26 -0300

    parte incial do prejeto pronta

---
+++ src/postagem/services/postagem.service.ts
@@ -0,0 +1,29 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Postagem } from '../entities/postagem.entity';
+
+@Injectable()
+export class PostagemService {
+  constructor(
+    @InjectRepository(Postagem)
+    private postagemRepository: Repository<Postagem>,
+  ) {}
+
+  async findAll(): Promise<Postagem[]> {
+    return await this.postagemRepository.find();
+  }
+
+  async findById(id: number): Promise<Postagem> {
+    const postagem = await this.postagemRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!postagem)
+      throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+    return postagem;
+  }
+}
