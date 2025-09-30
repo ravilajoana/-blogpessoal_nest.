061b222138a013332e3587f389826996c1e75ea4
ravila joana <ravilajoanasousapereira@gmail.com>
2025-09-30 15:41:08 -0300

    Atualizando projeto-ajustes

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
+import { TemaService } from '../../tema/services/tema.service';
 import { Postagem } from '../entities/postagem.entity';
 
 @Injectable()
@@ -8,10 +9,15 @@ export class PostagemService {
   constructor(
     @InjectRepository(Postagem)
     private postagemRepository: Repository<Postagem>,
+    private temaservice: TemaService,
   ) {}
 
   async findAll(): Promise<Postagem[]> {
-    return await this.postagemRepository.find();
+    return await this.postagemRepository.find({
+      relations: {
+        tema: true,
+      },
+    });
   }
 
   async findById(id: number): Promise<Postagem> {
@@ -19,11 +25,45 @@ export class PostagemService {
       where: {
         id,
       },
+      relations: {
+        tema: true,
+      },
     });
 
     if (!postagem)
       throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
 
     return postagem;
+  }
+
+  async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`),
+      },
+      relations: {
+        tema: true,
+      },
+    });
+  }
+
+  async create(postagem: Postagem): Promise<Postagem> {
+    await this.temaservice.findById(postagem.tema.id);
+
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async update(postagem: Postagem): Promise<Postagem> {
+    await this.findById(postagem.id);
+
+    await this.temaservice.findById(postagem.tema.id);
+
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+
+    return await this.postagemRepository.delete(id);
   }
 }
